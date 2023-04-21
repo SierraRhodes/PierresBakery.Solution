65a203b9104055f8b4e9d869b5ff11de82df414a
SierraRhodes <alreadywrote@gmail.com>
2023-04-20 21:37:41 -0700

    add instantiate bread test passed

--- Bakery.Tests/BreadTests.cs
+++ Bakery.Tests/BreadTests.cs
@@ -1,11 +1,20 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ProjectName.Models;
+using Bakery.Models;
 
 namespace Bakery.Tests
 {
   [TestClass]
   public class BreadTests
   {
-    // Test methods go here
+    [TestMethod]
+    public void Bread_InstantiateBread_ReturnBread()
+    {
+       Bread testBread = new Bread();
+       testBread.Price = 5.00m;
+       testBread.Quantity = 2;
+
+       Assert.AreEqual(5.00m, testBread.Price);
+       Assert.AreEqual(2, testBread.Quantity);
+    }
   }
 }
