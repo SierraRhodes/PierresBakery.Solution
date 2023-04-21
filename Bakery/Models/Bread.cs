f896c5a76c88d6026623f9986b85fc31c6160b85
SierraRhodes <alreadywrote@gmail.com>
2023-04-21 00:07:02 -0700

    Add calculate cost

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -1,7 +1,32 @@
-namespace ProjectName.Models
+namespace Bakery.Models
 {
-  public class ClassName
+  public class Bread
   {
-    // properties, constructors, methods, etc. go here
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+
+    public Bread (decimal Price, int Quantity)
+    {
+      this.Price = Price;
+      this.Quantity = Quantity;
+    }
+    public decimal GetPrice()
+    {
+      return Price;
+    }
+    public int GetQuantity()
+    {
+      return Quantity;
+    }
+    public decimal CalculateCost()
+    {
+      decimal pricePerLoaf = GetPrice();
+      int quantity = GetQuantity();
+      int discountLoaves = quantity / 2;
+      decimal freeLoaves = discountLoaves *  5.00m;
+      decimal cost = pricePerLoaf * quantity;
+      decimal totalCost = cost -= freeLoaves;
+      return totalCost;  
+    }
   }
 }
