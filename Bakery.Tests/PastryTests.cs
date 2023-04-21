6f38bdf1d819d5c9c31e7ac15bec5b211d2e97d3
SierraRhodes <alreadywrote@gmail.com>
2023-04-20 21:07:05 -0700

    set up project

---
+++ Bakery.Tests/PastryTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+namespace Pastry.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    // Test methods go here
+  }
+}
