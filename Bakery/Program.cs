eec2bc2f3d24e3ba673a4e7af25d56c384e94ee5
SierraRhodes <alreadywrote@gmail.com>
2023-04-21 18:26:43 -0700

    add user interface

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,7 +1,29 @@
+using System;
+using Bakery.Models;
+
+namespace Bakery.UI
+{
 class Program
 {
   static void Main()
   {
-    // We'll add user interface code here.
+    Console.WriteLine("Welcome to Pierre's Bakery!");
+    Console.WriteLine("Press any key to get started!");
+    Console.ReadKey();
+
+    Console.WriteLine("How many loaves of bread would you like to order?: ");
+    string breadInput = Console.ReadLine();
+    int breadQuantity = int.Parse(breadInput);
+
+    Console.WriteLine("How many pastries would you like to order?: ");
+    string pastryInput = Console.ReadLine();
+    int pastryQuantity = int.Parse(pastryInput); 
+
+    Bread bread = new Bread(5.00m, breadQuantity);
+    Pastry pastry = new Pastry(2.00m, pastryQuantity);
+    decimal orderTotal = bread.CalculateCost() + pastry.CalculateCost();
+
+    Console.WriteLine($"Your total amount due is ${orderTotal:F2}. Thank you for ordering with us!");
   }
+ }
 }
