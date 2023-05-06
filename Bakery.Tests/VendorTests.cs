716a28db80ee5b8f618db454b69aa374375be225
SierraRhodes <alreadywrote@gmail.com>
2023-05-05 19:19:54 -0700

    add vendor constructor test passed

--- Bakery.Tests/VendorTests.cs
+++ Bakery.Tests/VendorTests.cs
@@ -1,17 +1,20 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bakery.Models;
+using System;
 
 namespace Bakery.Tests
 {
   [TestClass]
   public class VendorTests
   {
-    // [TestMethod]
-    // public void Vendor_InstantiateVendor_ReturnVendor()
-    // {
-    //   Vendor testVendor = new Vendor("Rainbow Teahouse", "A teahouse");
+    [TestMethod]
+    public void Vendor_InstantiateVendor_ReturnVendor()
+    {
+      Vendor testVendor = new Vendor("Rainbow Teahouse", "A teahouse");
+      DateTime orderDate = new DateTime(2023, 5, 5);
+      Order testOrder = new Order("Croissant", 3, orderDate);
 
-
-    // }
+      Assert.IsNotNull(testVendor.Orders);
+    }
   }
 } 
