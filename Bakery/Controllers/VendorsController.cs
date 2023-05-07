d0c42b0e64cd493a5ba81bba308e5e19ac20d7b3
SierraRhodes <alreadywrote@gmail.com>
2023-05-06 17:44:53 -0700

    add clear all functionality

---
+++ Bakery/Controllers/VendorsController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Bakery.Models;
+using System.Collections.Generic;
+
+namespace Bakery.Controllers
+{
+ public class VendorsController : Controller
+ {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> vendors = Vendor.GetAll();
+      return View(vendors);
+    }
+  
+   [HttpGet("/vendors/new")]
+   public ActionResult New()
+   {
+    return View();
+   }
+   [HttpPost("/vendors")]
+   public ActionResult Create(string name, string description)
+   {
+     Vendor newVendor = new Vendor(name, description);
+     return RedirectToAction("Index");
+   }
+   [HttpPost("/vendors/clearall")]
+   public ActionResult ClearAll()
+   {
+    Vendor.ClearAll();
+    return RedirectToAction("Index");
+   }
+ }
+}
--- Bakery/Models/Vendor.cs
+++ Bakery/Models/Vendor.cs
@@ -7,6 +7,7 @@ namespace Bakery.Models
     public string Name { get; set; }
     public string Description { get; set; }
     public List<Order> Orders { get; set; } = new List<Order>();
+    private static List<Vendor> _instances = new List<Vendor>();
     public int OrderCount
     {
       get
@@ -18,7 +19,17 @@ namespace Bakery.Models
     {
       Name = name;
       Description = description;
+      _instances.Add(this);
     }
+    public static List<Vendor>GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
 
   }
 }
